0503e619ce046a78136af18140a2c40b2e9309bb
sagaringale10 <136690617+sagaringale10@users.noreply.github.com>
2023-06-23 14:15:57 +0800

    Update q1.js

--- src/q1.js
+++ src/q1.js
@@ -9,10 +9,26 @@
     - invoke the function "swap"
 */
 
-function swap(x, y){
-    // Task 1: Add code here
-}
 
-// Task 2: Add code here
+    // Task 1: 
+       def swap_values(x, y):
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        print("x and y must be numeric.")
+        return -1
+
+    x = x + y
+    y = x - y
+    x = x - y
+
+    print("Swapped values: x =", x, "y =", y)
+    return x, y
+
+
+
+// Task 2: 
+# Testing the function
+x = 5
+y = 10
+swap_values(x, y)
 
 module.exports = swap;
